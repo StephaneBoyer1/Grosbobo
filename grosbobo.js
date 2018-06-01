23354e92b377afc0b7dc8ee584062cb580f67c7b
StephaneBoyer1 <39792248+StephaneBoyer1@users.noreply.github.com>
2018-06-01 09:25:27 +0200

    Add files via upload

---
+++ grosbobo.js
@@ -0,0 +1,41 @@
+
+    	
+
+
+function tete(){
+	document.getElementById('para').innerHTML="<h6><i>Tête :</i></h6><p>La tête est la partie de l’anatomie du corps humain portée par le cou. L’homme, comme tous les primates, a une tête très différenciée, avec un cerveau important, un front, de grands yeux placés à l’avant, un nez, des joues, une bouche et deux oreilles sur chaque côté de la tête. Sa face est aplatie et particulièrement mobile : on l’appelle le visage. Au sommet de la tête se trouve la chevelure. Les os de la tête forment le crâne, dont la mâchoire porte la denture. La tête humaine augmente d'approximativement 15 % jusqu'à l'âge adulte. De plus, il est à noter que c'est lors de l'adolescence que cette poussée accrue s'effectue.</p>"
+	document.body.style.backgroundColor="White";
+}
+
+function epaule(){
+	document.getElementById('para').innerHTML="<h6>Epaule :</h6><p>L'épaule (nom féminin) est la région morphologique se situant à la jonction du tronc avec le membre supérieur.Elle comporte plusieurs articulations qui concourent à en faire le complexe articulaire le plus mobile du corps humain. Elle permet d'orienter le membre supérieur dans l'espace, autorisant en particulier son extrémité effectrice, la main, à assurer ses rôles de préhension et de communication avec l'environnement situé à sa portée.";
+	document.body.style.backgroundColor="White";
+}
+
+function coude(){
+	document.getElementById('para').innerHTML="<h6>Coude :</h6><p>Le coude (ou articulation olécranienne) est la partie du membre supérieur située entre le bras et l'avant-bras. Cette articulation comprend en avant la région du « pli du coude » (ou fosse cubitale). C'est un complexe articulaire synovial du membre supérieur humain reliant le bras à l'avant-bras. Il unit ainsi trois os entre eux : le radius, l'ulna (cubitus) et l'humérus</p>"
+	document.body.style.backgroundColor="White";
+}
+
+function main(){
+	document.getElementById('para').innerHTML="<h6>Main :</h6><p>La main (du latin : manus, « côté du corps1 ») est l’organe préhensile effecteur situé à l’extrémité de l’avant-bras et relié à ce dernier par le poignet. C'est un organe capable notamment de saisir et manipuler des objets. Chez l'homme, la main est un organe extrêmement développé et important, elle dispose d'une palette d'actions très large. Située à l'extrémité des deux membres supérieurs, chaque main possède cinq doigts qui apportent une contribution majeure au sens du toucher.</p>"
+	document.body.style.backgroundColor="White";
+}
+
+function hanche(){
+	document.getElementById('para').innerHTML="<h6>Hanche :</h6><p>La hanche ou articulation coxo-fémorale est une articulation (énarthrose) qui permet de joindre la cuisse au bassin. Elle met en jeu deux os : l'os iliaque et le fémur.</p>"
+	document.body.style.backgroundColor="White";
+}
+
+function genou(){
+	document.getElementById('para').innerHTML="<h6>Genou :</h6><p>Le genou est une articulation qui permet de joindre la jambe à la cuisse. Elle met en jeu trois os, le fémur, le tibia et la patella, par le biais de trois articulations, l'articulation fémoro-patellaire et la double articulation fémoro-tibiale. Le cartilage assure la fluidité des mouvements du genou. Le tissu élastique fin, le cartilage, protège l'os et fait en sorte que les surfaces de l'articulation glissent facilement les unes contre les autres. Le genou renferme deux types de cartilages articulaires: le cartilage fibreux (ménisque) et le cartilage hyalin.</p>"
+	document.body.style.backgroundColor = "White";
+}
+
+function pied(){
+	document.getElementById('para').innerHTML="<h6>Pied :</h6><p>Le pied est la partie distale du membre inférieur de l'homme. Il est relié à la jambe par l'articulation de la cheville. Il supporte le poids du corps en position debout et permet la locomotion.Premier élément au contact avec le sol — en position anatomique —, Il assure un rôle essentiel dans l'équilibre, l'amortissement et la propulsion.Le pied adulte comprend 26 os — soit, pour les deux pieds, le quart de ceux composant l'ensemble du squelette —, 16 articulations, 107 ligaments assurant leur protection et limitant la mobilité, 20 muscles qui permettent leur mouvement</p>"
+	document.body.style.backgroundColor = "White";
+}
+
+
+
